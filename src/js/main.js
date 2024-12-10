c3fc9987fd76360ac2eb71960aff49521c2fe25b
enjoylyingflat <chenpinchia@gmail.com>
2024-12-10 11:24:25 +0800

    content完成

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import '../assets/styles/all.css'; // 注意 ../ 回到上層資料夾
 import javascriptLogo from '../assets/images/javascript.svg';
 import viteLogo from '../assets/images/vite.svg';
 import { setupCounter } from '../assets/scripts/counter.js';
-import "./carousel.js";
+
 
 console.log("main已成功匯入！");
 
